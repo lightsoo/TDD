4b6a098353d94915d8b589c456f0444d9e56f558
김경수/Sell개발팀 <lightsoo@sk.com>
2018-08-04 16:29:47 +0900

    계좌 생성 및 잔고 조회

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,2 +1,12 @@
 public class Account {
+
+    private int balance;
+
+    public Account(int money) {
+        this.balance = money;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
 }
--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,9 +1,23 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class AccountTest {
     @Test
     public void createAccount() {
-        Account account = new Account();
+        Account account = new Account(20000);
+    }
+
+    @Test
+    public void getBalance() {
+        Account account = new Account(10000);
+        assertEquals("10000원 계좌 생성후 잔고 조회", 10000, account.getBalance());
+
+        account = new Account(100);
+        assertEquals("100원 계좌 생성후 잔고 조회", 100, account.getBalance());
+
+        account = new Account(0);
+        assertEquals("0원 계좌 생성후 잔고 조회", 0, account.getBalance());
     }
 }
 
